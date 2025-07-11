a8b7b2b681d7f1a73e0d13829b877bace6ba13c1
george-yeo <georgeyeo8998@gmail.com>
2025-07-11 20:15:06 +0800

    Update style

--- src/app/shared/issue-tooltip/issue-tooltip.component.ts
+++ src/app/shared/issue-tooltip/issue-tooltip.component.ts
@@ -1,16 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Issue } from '../../core/models/issue.model';
 
-export enum ACTION_BUTTONS {
-  VIEW_IN_WEB,
-  MARK_AS_RESPONDED,
-  MARK_AS_PENDING,
-  RESPOND_TO_ISSUE,
-  FIX_ISSUE,
-  DELETE_ISSUE,
-  RESTORE_ISSUE
-}
-
 @Component({
   selector: 'app-issue-tooltip',
   templateUrl: './issue-tooltip.component.html',
